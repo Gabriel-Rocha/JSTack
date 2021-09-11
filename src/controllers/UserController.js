934f1d711a48fcfb8c417fd270b29743406ac60d
Gabriel-Rocha <gabriel.santos15053@gmail.com>
2021-09-11 00:34:53 -0300

    criando o método response.send

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,8 +13,7 @@ module.exports = {
 
         });
 
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.end(JSON.stringify(sortedUsers));
+        response.send(200, sortedUsers);
     },
 
     getUsersById(request, response) {
@@ -23,14 +22,11 @@ module.exports = {
         const user = users.find((user) => user.id === Number(id));
 
         if (!user) {
-            
-        response.writeHead(400, {'Content-Type': 'application/json'});
-        response.end(JSON.stringify({ error: 'User not found' }));
-
-        } else {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.end(JSON.stringify(user));
-        }
+          return  response.send(200, { error: 'User not found' });
+        } 
+        
+        response.send(200, user);
+        
 
     }
 };
